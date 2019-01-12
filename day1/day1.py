e1fed85584b3c3c7618182fbf3e00bc2c1e8d61e
mangdehuang <1032036512@qq.com>
2019-01-12 13:47:39 +0800

    day1

--- day1/day1.py
+++ day1/day1.py
@@ -2,4 +2,17 @@ print('ssss');
 str = 'add love';
 print(str.title());
 
+str = ' add love ';
+print(str.lstrip());
+print(str.rstrip());
+print(str.strip());
 
+# 整数
+print(2*5);
+print(3 ** 2); # 幂运算
+
+# print ('happt '+ 25 +'bir'); ##报错
+print('happy ' + '25' + ' bir');
+
+print (3/2); # 1.5
+print (3.00 / 2); # 1.5
