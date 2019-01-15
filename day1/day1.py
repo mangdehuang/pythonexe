cf46039a9243dff3aa1b52de90c7b4e5ba3ffe75
mangdehuang <1032036512@qq.com>
2019-01-15 22:44:26 +0800

    day1

--- day1/day1.py
+++ day1/day1.py
@@ -1,18 +1,54 @@
-print('ssss');
-str = 'add love';
-print(str.title());
+# print('ssss');
+# str = 'add love';
+# print(str.title());
+#
+# str = ' add love ';
+# print(str.lstrip());
+# print(str.rstrip());
+# print(str.strip());
+#
+# # 整数
+# print(2*5);
+# print(3 ** 2); # 幂运算
+#
+# # print ('happt '+ 25 +'bir'); ##报错
+# print('happy ' + '25' + ' bir');
+#
+# print (3/2); # 1.5
+# print (3.00 / 2); # 1.5
 
-str = ' add love ';
-print(str.lstrip());
-print(str.rstrip());
-print(str.strip());
+# list
+# list = [1,2,3,4];
+# list.insert(1,'a');
+# list[0] = 5;
+# print(list);
+#
+# list = [1,2,3,4];
+# del list[0];
+# print(list);
 
-# 整数
-print(2*5);
-print(3 ** 2); # 幂运算
+# list = [1,2,3,4];
+# print(list.pop());
+# print(list);
+# print(list.pop(1));
+# print(list);
 
-# print ('happt '+ 25 +'bir'); ##报错
-print('happy ' + '25' + ' bir');
+# list = [1,2,3,4];
+# print(list.remove(2));
+# print(list);
 
-print (3/2); # 1.5
-print (3.00 / 2); # 1.5
+# list = [2,31,2,3,4];
+# list.sort();
+# print(list);
+# list.sort(reverse=True);
+# print(list);
+
+# list = [2,31,2,3,4];
+# list.reverse();
+# print(list);
+
+# list = [2,31,2,3,4];
+# print(len(list));
+
+# list = [2,31,2,3,4];
+# print(list[6]);
